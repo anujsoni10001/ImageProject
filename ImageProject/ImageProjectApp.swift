431fdef062725cd41c75a6260b28a9d4966bea90
Anuj Soni <anujsoni10001@gmail.com>
2022-03-21 18:49:09 +0530

    Initial Commit

---
+++ ImageProject/ImageProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageProjectApp.swift
+//  ImageProject
+//
+//  Created by Anuj Soni on 21/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ImageProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
