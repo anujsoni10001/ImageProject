9cfeaca75d164c688ed942862d852fa80acc88c9
Anuj Soni <anujsoni10001@gmail.com>
2022-03-22 10:22:31 +0530

    Understanding Image Animations

--- ImageProject/ContentView.swift
+++ ImageProject/ContentView.swift
@@ -4,13 +4,26 @@
 //
 //  Created by Anuj Soni on 21/03/22.
 //
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+@State private var isAnimating : Bool = false
+    
+    var body: some View{
+   
+        Image("IMG-20211118-WA0001")
+            .resizable()
+            .scaledToFit()
+            .clipShape(RoundedRectangle(cornerRadius:32))
+            .overlay(RoundedRectangle(cornerRadius: 32).stroke(Color.white.opacity(0.2),lineWidth:1))
+            .rotationEffect(Angle(degrees:isAnimating ? -12 : 0))
+            .scaleEffect(isAnimating ? 0.8 : 1)
+            .shadow(color:Color.black.opacity(0.77), radius:30,x:40,y:40)
+            .animation(.easeIn(duration:0.9), value:isAnimating)
+            .onAppear{
+                isAnimating.toggle()
+            }
     }
 }
 
@@ -19,3 +32,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
